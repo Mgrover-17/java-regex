c6108640a13d82b7d3db117c9cb0d94f5045d337
Manvi Grover <manvi0862.be21@chitkara.edu.in>
2025-04-03 11:11:06 +0530

    [MANVI GROVER] ADD: Validated SSN.

--- src/test/java/com/bridgelabz/advancedProblems/ValidateSSN.java
+++ src/test/java/com/bridgelabz/advancedProblems/ValidateSSN.java
@@ -1,4 +1,22 @@
 package com.bridgelabz.advancedProblems;
 
+import java.util.Scanner;
+import java.util.regex.*;
 public class ValidateSSN {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String number = "My SSN is 123-45-6789.";
+
+        String regex = "\\b[0-9]{3}-[0-9]{2}-[0-9]{4}\\b";
+
+        Pattern ssnPattern = Pattern.compile(regex);
+        Matcher matchSSN = ssnPattern.matcher(number);
+
+        if (matchSSN.find()) {
+            System.out.println(matchSSN.group() + " is a Valid SSN");
+        } else {
+            System.out.println(number + " is an Invalid SSN");
+        }
+    }
 }
