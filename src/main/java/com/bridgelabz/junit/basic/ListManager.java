351eeb9ec5383dd6419fc0c2635b386d3d51ece8
Manvi Grover <manvi0862.be21@chitkara.edu.in>
2025-04-04 00:00:32 +0530

    [MANVI GROVER] ADD: Checked test cases for list operations

--- src/main/java/com/bridgelabz/junit/basic/ListManager.java
+++ src/main/java/com/bridgelabz/junit/basic/ListManager.java
@@ -6,4 +6,12 @@ public class ListManager {
     public void addElement(List<Integer>list,int element){
         list.add(element);
     }
+    public void removeElement(List<Integer>list, int index){
+        if(index>=0 && index< list.size()) {
+            list.remove(index);
+        }
+    }
+    public int getSize(List<Integer>list){
+        return list.size();
+    }
 }
