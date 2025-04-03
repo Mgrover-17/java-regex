122c3c067e245569bbfeb211d499a4900016c72a
Manvi Grover <manvi0862.be21@chitkara.edu.in>
2025-04-04 00:58:22 +0530

    [MANVI GROVER] ADD: Testing Exception Handling.

--- src/test/java/com/bridgelabz/junit/basic/ExceptionHandlingTest.java
+++ src/test/java/com/bridgelabz/junit/basic/ExceptionHandlingTest.java
@@ -1,4 +1,20 @@
 package com.bridgelabz.junit.basic;
 
+import com.junit.basic.ExceptionHandling;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class ExceptionHandlingTest {
+    ExceptionHandling calculator=new ExceptionHandling();
+
+    @Test
+    public void shouldDivide(){
+        Assertions.assertEquals(2,calculator.divide(4,2));
+    }
+    @Test
+    public void shouldDivideByZeroTest(){
+        Assertions.assertThrows(ArithmeticException.class,()->{
+            calculator.divide(2,0);
+        }, "Expected Arithmetic Exception when dividing by zero.");
+    }
 }
