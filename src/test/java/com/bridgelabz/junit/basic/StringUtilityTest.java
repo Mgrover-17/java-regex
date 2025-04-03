0da5f1ce3ff51b29fa5956096d9ea355562fffe5
Manvi Grover <manvi0862.be21@chitkara.edu.in>
2025-04-04 00:39:22 +0530

    [MANVI GROVER] ADD: Testing String Utility methods.

--- src/test/java/com/bridgelabz/junit/basic/StringUtilityTest.java
+++ src/test/java/com/bridgelabz/junit/basic/StringUtilityTest.java
@@ -1,4 +1,24 @@
 package com.bridgelabz.junit.basic;
 
+import com.junit.basic.StringUtility;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class StringUtilityTest {
+    StringUtility stringUtilityMethods=new StringUtility();
+
+    @Test
+    public void shouldReverse(){
+        Assertions.assertEquals("olleh",stringUtilityMethods.reverse("hello"));
+    }
+
+    @Test
+    public void shouldPalindrome(){
+        Assertions.assertTrue(stringUtilityMethods.isPalindrome("madam"));
+    }
+
+    @Test
+    public void shouldUpperCase(){
+        Assertions.assertEquals("JAVA",stringUtilityMethods.toUpperCase("java"));
+    }
 }
