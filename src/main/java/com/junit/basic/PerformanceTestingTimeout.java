62670d24093f176ae9720bba4de70e1f20d665f0
Manvi Grover <manvi0862.be21@chitkara.edu.in>
2025-04-04 13:22:07 +0530

    [MANVI GROVER] ADD: Testing performance timeout.

--- src/main/java/com/junit/basic/PerformanceTestingTimeout.java
+++ src/main/java/com/junit/basic/PerformanceTestingTimeout.java
@@ -1,4 +1,13 @@
 package com.junit.basic;
 
 public class PerformanceTestingTimeout {
+    public static String longRunningTask(){
+        try{
+            //Thread.sleep(3000); fail
+            Thread.sleep(1500); //pass
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "Task completed";
+    }
 }
