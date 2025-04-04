01d49e96cbf99160529f6dee29050e18590fa996
Manvi Grover <manvi0862.be21@chitkara.edu.in>
2025-04-04 13:24:45 +0530

    [MANVI GROVER] ADD: Testing performance timeout.

--- src/test/java/com/bridgelabz/junit/basic/PerformanceTestingTimeoutTest.java
+++ src/test/java/com/bridgelabz/junit/basic/PerformanceTestingTimeoutTest.java
@@ -1,4 +1,16 @@
 package com.bridgelabz.junit.basic;
 
+import com.junit.basic.PerformanceTestingTimeout;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+import org.testng.Assert;
+
 public class PerformanceTestingTimeoutTest {
+
+    @Test
+    @Timeout(2)
+    void shouldTestPerformance(){
+        Assertions.assertEquals("Task completed", PerformanceTestingTimeout.longRunningTask());
+    }
 }
