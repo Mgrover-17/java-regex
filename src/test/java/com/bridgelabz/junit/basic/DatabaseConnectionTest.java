f468a75784ef7512f593a6066843ab8613910a1f
Manvi Grover <manvi0862.be21@chitkara.edu.in>
2025-04-04 12:03:14 +0530

    [MANVI GROVER] ADD: Testing database connection.

--- src/test/java/com/bridgelabz/junit/basic/DatabaseConnectionTest.java
+++ src/test/java/com/bridgelabz/junit/basic/DatabaseConnectionTest.java
@@ -1,4 +1,35 @@
 package com.bridgelabz.junit.basic;
 
+import com.junit.basic.DatabaseConnection;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class DatabaseConnectionTest {
+    DatabaseConnection db;
+
+    @BeforeEach
+    void setUp(){
+        //object create kro and establish a connection
+        db=new DatabaseConnection();
+        db.connect();
+    }
+
+    @AfterEach
+    void tearDown(){
+        //close the connection
+        db.disconnect();
+    }
+
+    @Test
+    void testConnectionEstablished(){
+        Assertions.assertTrue(db.isConnected,"Connection should be established.");
+    }
+
+    @Test
+    void testConnectionClosed() {
+        db.disconnect();
+        Assertions.assertFalse(db.isConnected, "Connection should be closed.");
+    }
 }
