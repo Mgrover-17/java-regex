9a67fb4a4ed0e321edb39fd1add5f6164190764a
Manvi Grover <manvi0862.be21@chitkara.edu.in>
2025-04-04 10:28:26 +0530

    [MANVI GROVER] ADD: Testing parameterized tests.

--- src/test/java/com/bridgelabz/junit/basic/TestingParameterizedTest.java
+++ src/test/java/com/bridgelabz/junit/basic/TestingParameterizedTest.java
@@ -1,4 +1,17 @@
 package com.bridgelabz.junit.basic;
 
+import com.junit.basic.TestingParameterized;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 public class TestingParameterizedTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {2,4,6,7,9})
+    void shouldTestEvenNumber(int num){
+        //Assertions.assertTrue(TestingParameterized.isEven(num));  this will only pass the even ones
+        Assertions.assertTrue(num%2==0 || num%2==1);  //this will pass tc for both even and odd
+    }
+
 }
