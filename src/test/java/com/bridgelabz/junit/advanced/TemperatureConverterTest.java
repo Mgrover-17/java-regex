da0af0b721c9bc6ca021bff83c21b6f4cb623f22
Manvi Grover <manvi0862.be21@chitkara.edu.in>
2025-04-05 13:19:16 +0530

    [MANVI GROVER] ADD: Testing temperature converter conversions.

--- src/test/java/com/bridgelabz/junit/advanced/TemperatureConverterTest.java
+++ src/test/java/com/bridgelabz/junit/advanced/TemperatureConverterTest.java
@@ -1,4 +1,17 @@
 package com.bridgelabz.junit.advanced;
 
+import com.junit.advanced.TemperatureConverter;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class TemperatureConverterTest {
+    @Test
+    void shouldConvertToFahrenheit(){
+        Assertions.assertEquals(77, TemperatureConverter.celsiusToFahrenheit(25));
+    }
+
+    @Test
+    void shouldConvertToCelsius(){
+        Assertions.assertEquals(25, TemperatureConverter.fahrenheitToCelsius(77));
+    }
 }
