51b85e9d3705cea2585e81cb31a50f01888619d3
Manvi Grover <manvi0862.be21@chitkara.edu.in>
2025-04-05 14:33:08 +0530

    [MANVI GROVER] ADD: Testing Date formatter.

--- src/test/java/com/bridgelabz/junit/advanced/DateFormatterTest.java
+++ src/test/java/com/bridgelabz/junit/advanced/DateFormatterTest.java
@@ -1,4 +1,38 @@
 package com.bridgelabz.junit.advanced;
 
+import com.junit.advanced.DateFormatter;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class DateFormatterTest {
+
+    @Test
+    void shouldTestValidDate(){
+        Assertions.assertEquals("04-04-2025", DateFormatter.formatDate("2025-04-04"));
+    }
+
+    @Test
+    void shouldTestInvalidFormat(){
+        Assertions.assertThrows(IllegalArgumentException.class,()->{
+            DateFormatter.formatDate("05/04/2025");
+        });
+    }
+
+    @Test
+    void shouldTestInvalidLogicalDate(){
+        Assertions.assertThrows(IllegalArgumentException.class,()->{
+            DateFormatter.formatDate("2025-02-30");
+        });
+    }
+
+    @Test
+    void shouldTestNullAndEmpty(){
+        Assertions.assertThrows(IllegalArgumentException.class,()->{
+            DateFormatter.formatDate("");
+        });
+
+        Assertions.assertThrows(IllegalArgumentException.class,()->{
+            DateFormatter.formatDate(null);
+        });
+    }
 }
